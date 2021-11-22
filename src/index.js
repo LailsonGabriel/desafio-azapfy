4b20bfac1996cc9398031930b098de2c847549c6
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-11-22 17:23:44 -0300

    desafio azapfy completo sem testes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyProviver from './provider/MyProviver';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <MyProviver>
+        <App />
+    </MyProviver>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
