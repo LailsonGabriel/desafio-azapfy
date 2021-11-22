77ffb3f152bd4266126f67df043a0370d5d40272
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-11-22 18:24:34 -0300

    ultima vez deploy

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('batata');
+const express = require('express');
+const app = express()
+const { resolve } = require('path')
+  app.use('/', express.static(
+    resolve(
+      __dirname,
+      './build'
+    )
+  )
+)
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) { return console.log(err); }
+  console.log('tudo funcionando certinho');
+});
